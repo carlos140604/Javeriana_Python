c5c66a3c2aac2299014980b4b463c3a09540cf33
carlos140604 <110997685+carlos140604@users.noreply.github.com>
2022-08-10 11:05:17 -0500

    Add files via upload

---
+++ 0x05-Functions.py
@@ -0,0 +1,13 @@
+def sumar(sumando1, sumando2):
+    print("resultado de la suma", sumando1+sumando2)
+sumar(4,5)
+
+def restar(minuendo, sustraendro):
+    print("resultado de la resta", minuendo-sustraendro)
+restar(20,10)
+def multiplicar(multiplicando, multiplicador):
+    print("resultado de la multiplicación", multiplicando*multiplicador)
+multiplicar(8,6)
+def potenciar(base,exponente):
+    print("resultado de la potencia", base**exponente)
+potenciar(2,3)
